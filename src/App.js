da7c75b2b9c894d3f2aa49dd1507d3698af27cce
Steven Kingston <skingston.dev@gmail.com>
2018-12-17 22:06:32 +0000

    Adds the missing depencies

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import SharedRates from "./routes/SharedRates";
 import Exchange from "./routes/Exchange";
 
+import reducers from "./redux/reducers";
+
 import "./App.scss";
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <div className="Route">
-            <Route exact path="/" component={Exchange} />
-            <Route path="/rates" component={SharedRates} />
+      <Provider store={createStoreWithMiddleware(reducers)}>
+        <Router>
+          <div className="App">
+            <div className="Route">
+              <Route exact path="/" component={Exchange} />
+              <Route path="/rates" component={SharedRates} />
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
