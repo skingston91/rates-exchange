cb0d8844096b513d37a04b68abe232909e0e7de8
Steven Kingston <skingston.dev@gmail.com>
2019-01-07 11:56:01 +0000

    Remove console.logs

--- src/api/currency.js
+++ src/api/currency.js
@@ -2,13 +2,9 @@
 import apiKey from "../data/apiKey";
 
 export function fetchCurrency(currency) {
-  console.log(currency);
   return fetch(
     `https://openexchangerates.org/api/latest.json?app_id=${apiKey}&base=${
       currency ? currency : "USD"
     }`
-  ).then(res => {
-    console.log(res);
-    return res.json();
-  });
+  ).then(res => res.json());
 }
