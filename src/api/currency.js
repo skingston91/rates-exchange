7b36f9578618a050a934d9e9561b27546cc7b91e
Steven Kingston <skingston.dev@gmail.com>
2019-01-13 22:50:41 +0000

    Updates the currency to not have a usd fallback
    Removes some todos
    Updates the README

--- src/api/currency.js
+++ src/api/currency.js
@@ -3,8 +3,6 @@ import apiKey from "../data/apiKey";
 
 export function fetchCurrency(currency) {
   return fetch(
-    `https://openexchangerates.org/api/latest.json?app_id=${apiKey}&base=${
-      currency ? currency : "USD"
-    }`
+    `https://openexchangerates.org/api/latest.json?app_id=${apiKey}&base=${currency}`
   ).then(res => res.json());
 }
